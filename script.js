9464a425d5794aeda0fdbf0ff312a6e17cd7c8be
liz <elizabeth.collacott2fcagroup.com>
2020-12-14 19:08:14 -0500

    help

--- script.js
+++ script.js
@@ -2,38 +2,34 @@
 
 let totalPurchases = 0;
 let purchaseArray = [];
-let newBalance = 0;
 let weeklyBudget = 0;
 let totalFood = 0;
 let totalBills = 0;
 let totalEntertainment = 0;
 let totalClothing = 0;
-
-
-document.querySelector("#budget").addEventListener("click", saveBudget);
-
 let weeklyInput = document.querySelector("#newbudget");
 let updateWeeklyBudget = document.querySelector("#Weekly-Budget");
-
-function saveBudget(e) {
-  e.preventDefault();
-  //console.log("having fun");
-  weeklyBudget = weeklyInput.value;
-  console.log(weeklyBudget + "budget");
-  updateWeeklyBudget.innerText = "$ " + weeklyBudget;
-  newBalance = weeklyBudget;
-  console.log(newBalance + "balance");
-}
-
-document.querySelector("#purchase").addEventListener("click", purchases);
-//document.querySelector("#purchase").addEventListener("click", balance);
-
 let purchaseName = document.querySelector("#name");
 let purchaseDescription = document.querySelector("#description");
 let purchasePrice = document.querySelector("#amount");
 let purchaseDate = document.querySelector("#date");
 let purchaseCategory = document.querySelector("#category");
 let purchaseList = document.querySelector("#purchase-list");
+let remainingBalance = document.querySelector("#Balance");
+
+document.querySelector("#budget").addEventListener("click", saveBudget);
+document.querySelector("#purchase").addEventListener("click", purchases);
+
+function saveBudget(e) {
+  e.preventDefault();
+  //console.log("having fun");
+  weeklyBudget = weeklyInput.value;
+  // console.log(weeklyBudget + "budget");
+  updateWeeklyBudget.innerText = "$ " + weeklyBudget;
+  newBalance = weeklyBudget;
+  // weeklyInput.value = '';
+  // console.log(newBalance + "balance");
+}
 
 // Charissa will work on completing this function
 function purchases(e) {
@@ -56,23 +52,29 @@ function purchases(e) {
   console.log("balance" + newBalance);
   totalPurchases = totalPurchases + parseInt(addPurchase.amount);
   console.log("total purchases" + totalPurchases);
-
   switch (addPurchase.category) {
-      case "Food": 
-        totalFood = totalFood + parseInt(addPurchase.amount);
-      case "Bills":
-        totalBills = totalBills + parseInt(addPurchase.amount);
-      case "Clothing":
-        totalClothing = totalClothing + parseInt(addPurchase.amount);
-      case "Entertainment":
-        totalEntertainment = totalEntertainment + parseInt(addPurchase.amount);      
-    console.log (totalFood + "total food");    
+    case "Food":
+      totalFood = totalFood + parseInt(addPurchase.amount);
+    case "Bills":
+      totalBills = totalBills + parseInt(addPurchase.amount);
+    case "Clothing":
+      totalClothing = totalClothing + parseInt(addPurchase.amount);
+    case "Entertainment":
+      totalEntertainment = totalEntertainment + parseInt(addPurchase.amount);
+      console.log(totalFood + "total food");
   }
-
+  updateBalance();
   console.log(totalFood + "total food");
-
 }
 
+function updateBalance() {
+  newBalance = newBalance - totalPurchases;
+  console.log(newBalance);
+  remainingBalance.innerText = "$" + newBalance;
+  if (totalPurchases === newBalance > 0) {
+    alert("Weekly budget consumed! Cannot purchase additional items.");
+  }
+}
 // Liz will complete the update balance function
 // Added balance function to purchase function
 
@@ -83,44 +85,35 @@ function purchases(e) {
 //  console.log = "total purchases" + totalPurchases;
 //}
 
+// Ashley worked on the pop up alert
+// Ashely, I moved this to the update balance function
+// if (totalPurchases === weeklyBudget > 0) {
+//   alert("Weekly budget consumed! Cannot purchase additional items.")
+// };
 // Ashley will start research on the progress bar
 
 // Asia will work on the category table
 
-// //Add purchase category and purchase amount entered by user into an array
-// var x = 0;
-// var array = Array();
+var entry = document.getElementById("submit");
+entry.addEventListener("click", displayCategoryDetails);
 
-// function add_element_to_array(){
-//     array[x] = purchaseCategory.value;
-//     array[x] = purchaseAmount.value;
-//  x++;
+var row = 1;
+function displayCategoryDetails() {
+  var purchaseCategory = document.getElementById("category").value;
+  var purchaseAmount = document.getElementById("amount").value;
 
-//  document.getElementById("category").value = "";
-//  document.getElementById("amount").value = "";
-// }
+  //create table
+  var display = document.getElementById("display");
+  //add new row
+  var newRow = display.insertRow(row);
+  //create cells
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  // var cell3 = newRow.insertCell(2);
 
-// //Display purchase amount and category
-// function display_array(){
-//    var e = "<hr/>";
+  cell1.innerHTML = purchaseCategory;
+  cell2.innerHTML = purchaseAmount;
+  // cell3.innerHTML = "";
 
-//    for (var y=0; y<array.length; y++){
-//        if (purchaseCategory == "Entertainment"){
-//            console.log(purchaseCatergory + purchaseAmount);
-//        }
-
-//         if (purchaseCategory == "Food"){
-//            console.log(purchaseCatergory + purchaseAmount);
-//        }
-
-//         if (purchaseCategory == "Clothing"){
-//            console.log(purchaseCatergory + purchaseAmount);
-//        }
-
-//         if (purchaseCategory == "Bills"){
-//           console.log(purchaseCatergory + purchaseAmount);
-
-//          }
-//     }
-
-// }
+  row++;
+}
